8b2430b631160563d856fd3fa26ef81f0eecdc5b
Prince Singh <princesingh6173@gmail.com>
2024-12-17 07:35:50 +0530

    DAY 1

---
+++ VARIABLES AND DATATYPES/first.js
@@ -0,0 +1,33 @@
+fullname = "Tony Stark"; //full name as variable 
+console.log(fullname); //printing full name on console 
+
+age = 24;
+console.log(age);
+
+price = 99;
+console.log(price);
+
+x = null //null means that x is storing a value but its not defined 
+console.log(x); 
+
+y = undefined ; //undefined means that console don't know what y is having or storing in it 
+console.log(y);
+
+isfollow = false; //boolean value {true or false only}
+
+// JAVA SCRIPT IS DYNAMICALLY TYPED LANGUAGE 
+
+let name = "tony stark"; //Variable cannot be re-declared but can be updated . A block scope variables 
+console.log(name);
+
+//NORMAL VARIBALE HOLDER CODE   
+const product ={
+    title :"Parker Ball Pen",
+    rating : 4,
+    offer : 5,
+    price : 270,
+};
+
+console.log(product);
+
+//To check its output , run the html file in browser > inspect it > click on console 
