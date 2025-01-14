c522ea4d7386e20e6f8dd03774d4c828b0759e44
Prince Singh <princesingh6173@gmail.com>
2025-01-14 17:29:21 +0530

    script.js

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,7 @@
 console.log("hello !"); //to print in console window 
 alert("LOVE");  //To generate alert pop up while surfing over any webpage 
+
+//WINDOW OBJECT 
+
+//The window object represents an open window in a browser . It is browser's object (not JavaScript's) & is automatically created by Browser 
+//It is a global object with lots of properties and methods
