51ca49f4874c4407d2abda7781469f4ffc7a92cc
Prince Singh <princesingh6173@gmail.com>
2025-01-14 17:34:04 +0530

    Update script.js

--- DOM/script.js
+++ DOM/script.js
@@ -5,3 +5,10 @@ alert("LOVE");  //To generate alert pop up while surfing over any webpage
 
 //The window object represents an open window in a browser . It is browser's object (not JavaScript's) & is automatically created by Browser 
 //It is a global object with lots of properties and methods
+
+console.log(window); //Printing window objects on console window 
+
+//window.alert('Love');
+
+//DOM : When a web page is loaded , the browser creates a Document object Model of the page 
+//HTML code is converted into special object named as Document which is part of JAVASCRIPT 
