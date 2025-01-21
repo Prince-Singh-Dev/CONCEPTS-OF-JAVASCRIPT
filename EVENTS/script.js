f08219702aed4780258b1805c04c53f060489eb2
Prince Singh <princesingh6173@gmail.com>
2025-01-21 05:41:50 +0530

    DAY 6

---
+++ EVENTS/script.js
@@ -0,0 +1,10 @@
+//EVENTS IN JS 
+
+// The change in the state of an object is known as an event
+// events are fired to notify code of "interesting changes" that may affect code execution
+
+// types of EVENTS :
+// 1. MOUSE EVENTS (click , double click)
+// 2. KEYBOARD EVENTS (keypress , keyup , keydown)
+// 3. FORM EVENTS (submit)
+// 4. PRINT EVENTS and many more 
