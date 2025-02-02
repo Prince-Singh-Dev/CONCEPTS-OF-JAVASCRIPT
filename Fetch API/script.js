fcb6f18b9c66d0be56bed0c4988af58d2148bc6c
Prince Singh <princesingh6173@gmail.com>
2025-02-02 14:40:51 +0530

    DAY 9

--- Fetch API/script.js
+++ Fetch API/script.js
@@ -3,3 +3,18 @@
 //The fetch() mathod is used to fetch a resource (data).
 //let promise = fetch(url,[options])
 //API : Application Programming Interface 
+
+const URL ="https://alexwohlbruck.github.io/cat-facts/";  //Free api link from Browser 
+
+const getFacts = async() => {
+    console.log("getting data ....");
+    let response = await fetch (URL);  //Getting a response from Fetch API function 
+    console.log(response); //Printing the response 
+    console.log(response.status); //Printing response status 
+}
+
+//Understanding Some IMP Terms 
+
+//AJAX is Asynchronous JS & XML
+//JSON is Javascript Object Notation 
+//json() method : returns a second promise that resolves with result of parsing the response body text as JSON . (Input is JSON , Output is JS object)
