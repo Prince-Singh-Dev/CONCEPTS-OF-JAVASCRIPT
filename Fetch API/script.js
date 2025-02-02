0717be94663b24a7c3977437b12d5aae6733820f
Prince Singh <princesingh6173@gmail.com>
2025-02-02 14:18:00 +0530

    DAY 9

---
+++ Fetch API/script.js
@@ -0,0 +1,5 @@
+//fetch API 
+//The fetch API provides an interface for fetching (sending/receiving) resources . It uses Request and Response objects 
+//The fetch() mathod is used to fetch a resource (data).
+//let promise = fetch(url,[options])
+//API : Application Programming Interface 
